56d024de4430296fe81e6396b3ad15d7571257d1
trung <trung@nimblehq.co>
2021-03-23 18:26:05 +0700

    #4 Add login form

---
+++ lib/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    primaryColor: Colors.white,
+    textTheme: TextTheme(
+        headline5:
+            TextStyle(color: Colors.white, fontWeight: FontWeight.bold)));
