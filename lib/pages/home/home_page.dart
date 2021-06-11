5aa4275dfb0b8815e094c93a94760696e372d195
trung <trung@nimblehq.co>
2021-06-11 17:05:19 +0700

    [#57] Fix placeholder asset

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:survey/pages/home/home_controller.dart';
 
@@ -40,10 +39,12 @@ class HomePage extends StatelessWidget {
                         ),
                         CircleAvatar(
                           radius: 22,
-                          child: state.currentUserAvatar.value.isNotEmpty
-                              ? NetworkImage(state.currentUserAvatar.value)
-                              : SvgPicture.asset(
-                                  'assets/images/avatar_placeholder.svg'),
+                          backgroundColor: Colors.black12,
+                          foregroundImage:
+                              state.currentUserAvatar.value.isNotEmpty
+                                  ? NetworkImage(state.currentUserAvatar.value)
+                                  : AssetImage(
+                                      'assets/images/avatar_placeholder.png'),
                         ),
                       ],
                     );
