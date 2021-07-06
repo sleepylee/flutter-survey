b7bd8787fe1f6482a33a550aa68d2db135a4490f
trung <trung@nimblehq.co>
2021-07-06 17:58:42 +0700

    [#21] Integrate survey detail to the UI

--- lib/pages/survey/survey_controller.dart
+++ lib/pages/survey/survey_controller.dart
@@ -1,8 +1,15 @@
 import 'package:get/get.dart';
+import 'package:optional/optional.dart';
+import 'package:survey/models/survey.dart';
 import 'package:survey/navigator/const.dart';
+import 'package:survey/use_cases/base_use_case.dart';
 import 'package:survey/use_cases/get_survey_detail_use_case.dart';
 
 class SurveyController extends GetxController {
+  final Rx<Optional<Survey>> _survey = Optional<Survey>.empty().obs;
+
+  Optional<Survey> get optionalSurvey => _survey.value;
+
   @override
   void onInit() {
     super.onInit();
@@ -10,6 +17,9 @@ class SurveyController extends GetxController {
     final surveyId = Get.arguments[DATA_SURVEY_ID];
     // TODO: remove this when integrate
     final getSurveyUseCase = Get.find<GetSurveyDetailUseCase>();
-    getSurveyUseCase.call(surveyId).then((value) => print("Value: $value"));
+    getSurveyUseCase.call(surveyId).then((result) => {
+          if (result is Success<Survey>)
+            {_survey.value = Optional.of(result.value)}
+        });
   }
 }
