350da1f30c90a056227c6918c6688164812ce437
trung <trung@nimblehq.co>
2021-07-06 17:00:26 +0700

    [#20] Update dependency management

--- lib/pages/survey/survey_binding.dart
+++ lib/pages/survey/survey_binding.dart
@@ -1,13 +1,11 @@
 import 'package:get/get.dart';
-import 'package:survey/api/graphql/graphql_client_provider.dart';
 import 'package:survey/repositories/survey_repository.dart';
 import 'package:survey/use_cases/get_survey_detail_use_case.dart';
 
 class SurveyBinding implements Bindings {
   @override
   void dependencies() {
-    final graphQLClientProvider = Get.find<GraphQLClientProvider>();
-    final surveyRepository = SurveyRepositoryImpl(graphQLClientProvider.client);
-    Get.put(GetSurveyDetailUseCase(surveyRepository));
+    final surveyRepository = Get.find<SurveyRepository>();
+    Get.lazyPut(() => GetSurveyDetailUseCase(surveyRepository));
   }
 }
