97d750ffadc444fa44385a4bd36b69fdef362823
trung <trung@nimblehq.co>
2021-08-01 21:16:27 +0700

    #54 Integrate auto-refresh token

--- lib/api/graphql/graphql_client_provider.dart
+++ lib/api/graphql/graphql_client_provider.dart
@@ -1,4 +1,14 @@
+import 'package:fresh_graphql/fresh_graphql.dart';
+import 'package:get/get.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:survey/api/graphql/const.dart';
+import 'package:survey/api/graphql/link/custom_refresh_link.dart';
+import 'package:survey/api/http/api_client.dart';
+import 'package:survey/models/auth_token.dart';
+import 'package:survey/preferences/shared_preferences.dart';
+import 'package:survey/repositories/oauth_repository.dart';
+import 'package:survey/use_cases/base_use_case.dart';
+import 'package:survey/use_cases/refresh_token_use_case.dart';
 
 import '../../flavors.dart';
 
@@ -8,6 +18,8 @@ class GraphQLClientProvider {
   const GraphQLClientProvider._(this._httpLink);
 
   static AuthLink _authLink;
+  static CustomRefreshLink _refreshLink;
+  static GraphQLClient _client;
 
   factory GraphQLClientProvider() {
     return GraphQLClientProvider._(
@@ -15,28 +27,58 @@ class GraphQLClientProvider {
     );
   }
 
-  // Setup a token to all request i.e: 'Bearer - token'
+  GraphQLClient get client {
+    if (_client == null) {
+      Link _link;
+      if (_authLink != null) {
+        _link = Link.from([_authLink, _refreshLink, _httpLink]);
+      } else {
+        _link = _httpLink;
+      }
+      _client = GraphQLClient(
+        cache: GraphQLCache(),
+        link: _link,
+      );
+    }
+    return _client;
+  }
+
+  /// Setup a token to all request i.e: 'Bearer - token'
+  /// Also set up the refresh token [Link]
   void setAuthToken(String token) {
     _authLink = AuthLink(
       getToken: () async => token,
+    );
+
+    _refreshLink = CustomRefreshLink.oAuth2(
+      tokenStorage: LocalSharedPreferencesStorage(),
+      doRefreshing: (oauth2Token) async {
+        final httpClient = Get.find<ApiClient>();
+        final oauthRepository = OAuthRepositoryImpl(httpClient);
+        final refreshTokenUseCase = RefreshTokenUseCase(oauthRepository);
+
+        final result = await refreshTokenUseCase.call(oauth2Token.refreshToken);
+        if (result is Success<AuthToken>) {
+          return OAuth2Token(
+            accessToken: result.value.accessToken,
+            refreshToken: result.value.refreshToken,
+            expiresIn: result.value.expiresIn,
+            tokenType: result.value.tokenType,
+          );
+        } else {
+          return null;
+        }
+      },
+      shouldRefresh: (response) {
+        var hasAuthError = response.errors != null &&
+            response.errors.any((element) =>
+                element.extensions['code'] == CODE_INVALID_OR_EXPIRED_TOKEN);
+        return hasAuthError;
+      },
     );
   }
 
   void removeAuthToken() {
     _authLink = null;
-  }
-
-  GraphQLClient get client {
-    Link _link;
-    if (_authLink != null) {
-      _link = _authLink.concat(_httpLink);
-    } else {
-      _link = _httpLink;
-    }
-
-    return GraphQLClient(
-      cache: GraphQLCache(),
-      link: _link,
-    );
   }
 }
--- lib/use_cases/get_login_state_use_case.dart
+++ lib/use_cases/get_login_state_use_case.dart
@@ -11,17 +11,13 @@ class GetLoginStateUseCase extends NoParamsUseCase<bool> {
 
   @override
   Future<Result<bool>> call() async {
-    final token = await _sharedPreferencesStorage.getAccessToken();
+    final refreshToken = await _sharedPreferencesStorage.getRefreshToken();
+    final accessToken = await _sharedPreferencesStorage.getAccessToken();
     final tokenType = await _sharedPreferencesStorage.getTokenType();
-    final tokenExpiration =
-        await _sharedPreferencesStorage.getTokenExpiration();
-    final hasValidTokenStored = token != null &&
-        token.isNotEmpty &&
-        tokenExpiration > DateTime.now().millisecondsSinceEpoch;
+    final hasValidTokenStored = refreshToken != null && refreshToken.isNotEmpty;
 
     if (hasValidTokenStored) {
-      // TODO: create an Authenticator instead of calling directly like this
-      _graphQLClientProvider.setAuthToken("$tokenType $token");
+      _graphQLClientProvider.setAuthToken("$tokenType $accessToken");
     }
     return Success(hasValidTokenStored);
   }
