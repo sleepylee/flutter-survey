54991c91e7c770a7bfdd156bf0b3949817829f6c
trung <trung@nimblehq.co>
2021-08-01 21:28:52 +0700

    #53 Remove unnecessary access via repo & usecase

--- lib/repositories/oauth_repository.dart
+++ lib/repositories/oauth_repository.dart
@@ -1,13 +1,10 @@
 import 'package:survey/api/http/api_client.dart';
 import 'package:survey/api/http/request/oauth_token_request.dart';
-import 'package:survey/api/http/request/refresh_token_request.dart';
 import 'package:survey/flavors.dart';
 import 'package:survey/models/auth_token.dart';
 
 abstract class OAuthRepository {
   Future<AuthToken> login(String email, String password);
-
-  Future<AuthToken> refreshToken(String refreshToken);
 }
 
 class OAuthRepositoryImpl implements OAuthRepository {
@@ -21,24 +18,6 @@ class OAuthRepositoryImpl implements OAuthRepository {
         .login(OAuthTokenRequest(
             email: email,
             password: password,
-            clientId: F.basicAuthClientId,
-            clientSecret: F.basicAuthClientSecret))
-        .then((value) {
-      final authTokenResponse = value.data.authToken;
-      return AuthToken(
-        accessToken: authTokenResponse.accessToken,
-        tokenType: authTokenResponse.tokenType,
-        refreshToken: authTokenResponse.refreshToken,
-        expiresIn: authTokenResponse.expiresIn,
-      );
-    });
-  }
-
-  @override
-  Future<AuthToken> refreshToken(String refreshToken) {
-    return _restApiClient
-        .refreshToken(RefreshTokenRequest(
-            refreshToken: refreshToken,
             clientId: F.basicAuthClientId,
             clientSecret: F.basicAuthClientSecret))
         .then((value) {
